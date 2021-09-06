ab16f1c9718f59823d047ea4353e07dfa74e74bb
ProfessorDC <81577983+ProfessorDC@users.noreply.github.com>
2021-09-06 15:48:32 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,8 +81,10 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_name} <br> 📁{file_size} "
+          #  button_text = f"{file_size} 📁 {file_name}"
        
+             button_text = f"{file_name} 📁"
+             button_text = f"{file_size:same}"
             
 
             if file_type == "video":

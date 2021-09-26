c1a142307899d7a34ffbdf8a3db261e4597a526e
ProfessorDC <81577983+ProfessorDC@users.noreply.github.com>
2021-09-26 17:43:58 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -211,7 +211,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f".:⭐ Check all Links At @TheMoviesWorldWide \n @worldmoviesnew ⭐:. \n Found {(len_results)} Results For Your Search: <code>{query}</code>",
+                text=f".:⭐ Check all Links and for more moviews here : \n @TheMoviesWorldWide \n @worldmoviesnew ⭐:. \n Found {(len_results)} Results For Your Search: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
